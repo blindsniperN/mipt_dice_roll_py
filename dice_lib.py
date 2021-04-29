13547726bd5d4fa8c3a59f5c2d3b9d5f40cfcd15
blindsniperN <spitsyn.na@phystech.edu>
2021-04-29 18:10:33 +0300

    commit fixes - refactored code according to PEP-8 - added extra comments to describe functions - added requirements.txt

--- dice_lib.py
+++ dice_lib.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from random import randint
+from numpy import percentile
 
 
 def expected_value(x):  # подсчёт мат ожидания
@@ -10,7 +11,7 @@ def expected_value(x):  # подсчёт мат ожидания
         return summa / sum(x)
 
 
-def turn_to_probability(y, acc):
+def turn_list_to_probability(y, acc):
     if y is not None:
         sm = sum(y)
         x = [round(number / sm, acc) for number in y]
@@ -23,9 +24,10 @@ def plt_probability(y):  # построение графика
         plt.show()
 
 
-def geqslant(arr, modif, x):
+# на вход последующим трём функциям подаётся массив исходов, а на выходе - одно значение - возможность выкинуть
+def probability_to_roll_greater_or_equal(arr, value, x):
     if arr is not None:
-        if x < modif:
+        if x < value:
             return 1
         elif x > len(arr) - 1:
             return 0
@@ -33,9 +35,9 @@ def geqslant(arr, modif, x):
             return sum(arr[x:]) / sum(arr)
 
 
-def leqslant(arr, modif, x):
+def probability_to_roll_less_or_equal(arr, value, x):
     if arr is not None:
-        if x < modif:
+        if x < value:
             return 0
         elif x >= len(arr) - 1:
             return 1
@@ -43,9 +45,9 @@ def leqslant(arr, modif, x):
             return sum(arr[:x+1]) / sum(arr)
 
 
-def eq(arr, modif, x):
+def probability_to_roll_equal(arr, value, x):
     if arr is not None:
-        if x < modif or x > len(arr) - 1:
+        if x < value or x > len(arr) - 1:
             return 0
         else:
             return arr[x] / sum(arr)
@@ -67,39 +69,41 @@ class DiceRoll:
         self.modifier_ = 0  # модификатор броска
         self.add(s)
 
-    def add(self, s):
-        if s != '':
-            s += ' + '
-            while s != '':
-                if (s[:s.find(' ')]).find('d') != -1:
-                    amount = int(s[:s.find('d')])
-                    s = s[s.find('d') + 1:]
-                    cap = int(s[:s.find(' ')])
+    # str_inp - ввод кубов в short notation записи: 1d6, 2d20 + 5. add - добавляет эти кубы в запись, minus - удаляет
+    def add(self, str_inp):
+        if str_inp != '':
+            str_inp += ' + '
+            while str_inp != '':
+                if (str_inp[:str_inp.find(' ')]).find('d') != -1:
+                    amount = int(str_inp[:str_inp.find('d')])
+                    str_inp = str_inp[str_inp.find('d') + 1:]
+                    cap = int(str_inp[:str_inp.find(' ')])
                     if cap in self.diceN_:
                         self.diceN_[cap] += amount
                     else:
                         self.diceN_[cap] = amount
-                elif s != ' ':
-                    self.modifier_ += int(s[:s.find(' ')])
-                s = s[s.find('+') + 2:]
+                elif str_inp != ' ':
+                    self.modifier_ += int(str_inp[:str_inp.find(' ')])
+                str_inp = str_inp[str_inp.find('+') + 2:]
 
-    def minus(self, s):
-        s += ' + '
-        while s != '':
-            if s[:s.find(' ')].find('d') != -1:
-                amount = int(s[:s.find('d')])
-                s = s[s.find('d') + 1:]
-                cap = int(s[:s.find(' ')])
-                s = s[s.find('+') + 2:]
+    def minus(self, str_inp):
+        str_inp += ' + '
+        while str_inp != '':
+            if str_inp[:str_inp.find(' ')].find('d') != -1:
+                amount = int(str_inp[:str_inp.find('d')])
+                str_inp = str_inp[str_inp.find('d') + 1:]
+                cap = int(str_inp[:str_inp.find(' ')])
+                str_inp = str_inp[str_inp.find('+') + 2:]
                 if cap in self.diceN_:
                     if self.diceN_[cap] <= amount:
                         self.diceN_[cap] = 0
                     else:
                         self.diceN_[cap] -= amount
-            elif s != ' ':
-                self.modifier_ -= int(s[:s.find(' ')])
-            s = s[s.find('+') + 2:]
+            elif str_inp != ' ':
+                self.modifier_ -= int(str_inp[:str_inp.find(' ')])
+            str_inp = str_inp[str_inp.find('+') + 2:]
 
+    # вывод всех текущих кубов в классе
     def print(self):
         s = ''
         for el in sorted(self.diceN_.items()):
@@ -114,16 +118,18 @@ class DiceRoll:
         else:
             print('0 dice added')
 
+    # возвращает текущие кубы в классе диктом
     def return_dict(self):
         return self.diceN_
 
+    # возвращает максимальное значение, которое может выпасть на кубах
     def max_res(self):
         sum_d = 0
         for el in self.diceN_.items():
             sum_d += el[1]*el[0]
         return sum_d + self.modifier_
 
-    def arr_sum(self):  # подсчёт массива всех возможных величин
+    def list_sum(self):  # подсчёт массива всех возможных величин при обычном броске куба
         x = [0] * (self.max_res() + 1)
         x[self.modifier_] = 1
         for el in self.diceN_.items():
@@ -135,7 +141,9 @@ class DiceRoll:
                 x = cpy
         return x
 
-    def adv(self):
+    # лист количества возможных исходов (значений на кубах) при броске с преимуществом: выбирается максимум из того, что
+    # выпало на кубах
+    def lst_outcomes_roll_with_advantage(self):
         if len(self.diceN_) != 1:
             print('Cannot calculate that')
             return None
@@ -151,7 +159,9 @@ class DiceRoll:
                     x = cpy
                 return x
 
-    def dis(self):
+    # лист количества возможных исходов (значений на кубах) при броске с помехой: выбирается минимум из того, что
+    # выпало на кубах
+    def lst_outcomes_roll_with_disadvantage(self):
         if len(self.diceN_) != 1:
             print('Cannot calculate that')
             return None
@@ -167,7 +177,8 @@ class DiceRoll:
                     x = cpy
                 return x
 
-    def arr_sum_reroll_less(self, least_reroll):  # подсчёт массива всех возможных величин
+    # лист количества возможных исходов (значений на кубах) при перебрасывании кубов, если на них выпало <= value
+    def lst_outcomes_roll_again_less_or_equal(self, value):  # подсчёт массива всех возможных величин
         x = [0] * (self.max_res() + 1)
         x[self.modifier_] = 1
         for el in self.diceN_.items():
@@ -175,15 +186,15 @@ class DiceRoll:
                 cpy = [0] * (self.max_res() + 1)
                 for j in range(len(x) - el[0]):
                     for k in range(1, el[0] + 1):
-                        if k > least_reroll:
+                        if k > value:
                             cpy[j + k] += x[j] * el[0]
                         else:
-                            for reroll in range(1, el[0] + 1):
-                                cpy[j + reroll] += x[j]
+                            for dice_result in range(1, el[0] + 1):
+                                cpy[j + dice_result] += x[j]
                 x = cpy
         return x
 
-    def bmb(self, depth):
+    def witcher_explosive_dice_probabilities(self, depth):
         if len(self.diceN_) != 1:
             print('Cannot calculate that')
             return None
@@ -199,3 +210,106 @@ class DiceRoll:
                             arr[self.modifier_ + i * el[0] + j] = el[0] ** (depth - i - 1)
                     arr[len(arr) - 1] = 1
                     return arr
+
+
+def prob_preparation(d, s):  # предобработка кубов для команд r>=, r==, r<=
+    y = 0
+    arr = []
+    if s[4:7] == 'sum':
+        arr = d.list_sum()
+        y = int(s[8:])
+    elif s[4:7] == 'adv':
+        arr = d.lst_outcomes_roll_with_advantage()
+        y = int(s[8:])
+    elif s[4:7] == 'dis':
+        arr = d.lst_outcomes_roll_with_disadvantage()
+        y = int(s[8:])
+    elif s[4:7] == 'rrl':
+        x, y = map(int, s[8:].split())
+        arr = d.lst_outcomes_roll_again_less_or_equal(x)
+    elif s[4:7] == 'bmb':
+        x, y = map(int, s[8:].split())
+        arr = d.witcher_explosive_dice_probabilities(x)
+    return [arr, y]
+
+
+def preparation(d, s):  # предобработка кубов для прочих команд
+    arr = []
+    if s[4:7] == 'sum':
+        arr = d.list_sum()
+    elif s[4:7] == 'adv':
+        arr = d.lst_outcomes_roll_with_advantage()
+    elif s[4:7] == 'dis':
+        arr = d.lst_outcomes_roll_with_disadvantage()
+    elif s[4:7] == 'rrl':
+        x = int(s[8:])
+        arr = d.lst_outcomes_roll_again_less_or_equal(x)
+    elif s[4:7] == 'bmb':
+        arr = d.witcher_explosive_dice_probabilities(int(s[8:]))
+    return arr
+
+
+def rolling_dice(d, s, accuracy):
+    cmd = s[:3]
+    if cmd == 'add':  # добавить кубы
+        d.add(s[4:])
+
+    elif cmd == 'del':  # удалить кубы
+        d.minus(s[4:])
+
+    elif cmd == 'max':
+        return 'Max sum on dice: ' + str(d.max_res())
+
+    elif cmd == 'arr':  # массив кубов
+        return preparation(d, s)
+
+    elif cmd == 'r>=':
+        arr, y = prob_preparation(d, s)
+        res = probability_to_roll_greater_or_equal(arr, d.modifier_, y)
+        if res is not None:
+            return 'Probability is ' + str(round(res, accuracy))
+
+    elif cmd == 'r<=':
+        arr, y = prob_preparation(d, s)
+        res = probability_to_roll_less_or_equal(arr, d.modifier_, y)
+        if res is not None:
+            return 'Probability is ' + str(round(res, accuracy))
+
+    elif cmd == 'r==':
+        arr, y = prob_preparation(d, s)
+        res = probability_to_roll_equal(arr, d.modifier_, y)
+        if res is not None:
+            return 'Probability is ' + str(round(res, accuracy))
+
+    elif cmd == 'xpv':  # мат ожидание
+        res = expected_value(preparation(d, s))
+        if res is not None:
+            return round(res, accuracy)
+
+    elif cmd == 'prb':
+        return turn_list_to_probability(preparation(d, s), accuracy)
+
+    elif cmd == 'plt':
+        plt_probability(turn_list_to_probability(preparation(d, s), accuracy))
+
+    elif cmd == 'rll':
+        return roll(preparation(d, s))
+
+    elif cmd == 'prc':
+        arr, y = prob_preparation(d, s)
+        if arr is not None:
+            return str(y) + '% percentile is: ' + str(percentile(arr, y))
+
+
+# лист мат ожиданий урона в заивисимости от значения ac
+# critical hit - критическое попадание, попадание, при котором результат на кубах удваивается
+def expected_dmg_per_ac(to_hit, to_hit_mode, dmg, dmg_mode, are_there_critical_hits, ac, accuracy):
+    arr = rolling_dice(to_hit, 'arr ' + to_hit_mode, accuracy)
+    xpv = rolling_dice(dmg, 'xpv ' + dmg_mode, accuracy)
+    prob_sum = 0
+    for i in range(len(arr)):
+        if (arr[i] == len(arr) - 1) and (are_there_critical_hits == 'YES') and (i >= ac):
+            prob_sum += arr[i] * (xpv * 2 - dmg.modifier_)
+        elif i >= ac:
+            prob_sum += arr[i] * xpv
+    return round(prob_sum / sum(arr), accuracy)
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from dice_lib import *
-from numpy import percentile
 
 
 accuracy = 3
@@ -34,111 +33,11 @@ def cycle_dice():
             global accuracy
             accuracy = int(s[4:])
         else:
-            check = rolling_dice(d, s)
+            global accuracy
+            check = rolling_dice(d, s, accuracy)
             if check is not None:
                 print(check)
             d.print()
-
-
-def prob_preparation(d, s):  # предобработка кубов для команд r>=, r==, r<=
-    y = 0
-    arr = []
-    if s[4:7] == 'sum':
-        arr = d.arr_sum()
-        y = int(s[8:])
-    elif s[4:7] == 'adv':
-        arr = d.adv()
-        y = int(s[8:])
-    elif s[4:7] == 'dis':
-        arr = d.dis()
-        y = int(s[8:])
-    elif s[4:7] == 'rrl':
-        x, y = map(int, s[8:].split())
-        arr = d.arr_sum_reroll_less(x)
-    elif s[4:7] == 'bmb':
-        x, y = map(int, s[8:].split())
-        arr = d.bmb(x)
-    return [arr, y]
-
-
-def preparation(d, s):  # предобработка кубов для прочих команд
-    arr = []
-    if s[4:7] == 'sum':
-        arr = d.arr_sum()
-    elif s[4:7] == 'adv':
-        arr = d.adv()
-    elif s[4:7] == 'dis':
-        arr = d.dis()
-    elif s[4:7] == 'rrl':
-        x = int(s[8:])
-        arr = d.arr_sum_reroll_less(x)
-    elif s[4:7] == 'bmb':
-        arr = d.bmb(int(s[8:]))
-    return arr
-
-
-def rolling_dice(d, s):
-    cmd = s[:3]
-    if cmd == 'add':  # добавить кубы
-        d.add(s[4:])
-
-    elif cmd == 'del':  # удалить кубы
-        d.minus(s[4:])
-
-    elif cmd == 'max':
-        return 'Max sum on dice: ' + str(d.max_res())
-
-    elif cmd == 'arr':  # массив кубов
-        return preparation(d, s)
-
-    elif cmd == 'r>=':
-        arr, y = prob_preparation(d, s)
-        res = geqslant(arr, d.modifier_, y)
-        if res is not None:
-            return 'Probability is ' + str(round(res, accuracy))
-
-    elif cmd == 'r<=':
-        arr, y = prob_preparation(d, s)
-        res = leqslant(arr, d.modifier_, y)
-        if res is not None:
-            return 'Probability is ' + str(round(res, accuracy))
-
-    elif cmd == 'r==':
-        arr, y = prob_preparation(d, s)
-        res = eq(arr, d.modifier_, y)
-        if res is not None:
-            return 'Probability is ' + str(round(res, accuracy))
-
-    elif cmd == 'xpv':  # мат ожидание
-        res = expected_value(preparation(d, s))
-        if res is not None:
-            return round(res, accuracy)
-
-    elif cmd == 'prb':
-        return turn_to_probability(preparation(d, s), accuracy)
-
-    elif cmd == 'plt':
-        plt_probability(turn_to_probability(preparation(d, s), accuracy))
-
-    elif cmd == 'rll':
-        return roll(preparation(d, s))
-
-    elif cmd == 'prc':
-        arr, y = prob_preparation(d, s)
-        if arr is not None:
-            return str(y) + '% percentile is: ' + str(percentile(arr, y))
-
-
-def expected_dmg_per_ac(to_hit, to_hit_mode, dmg, dmg_mode, crit, ac):
-    arr = rolling_dice(to_hit, 'arr ' + to_hit_mode)
-    xpv = rolling_dice(dmg, 'xpv ' + dmg_mode)
-    prob_sum = 0
-    for i in range(len(arr)):
-        if (arr[i] == len(arr) - 1) and (crit == 'YES') and (i >= ac):
-            prob_sum += arr[i] * (xpv * 2 - dmg.modifier_)
-        elif i >= ac:
-            prob_sum += arr[i] * xpv
-    return round(prob_sum / sum(arr), accuracy)
 
 
 def character_build():
@@ -152,14 +51,17 @@ def character_build():
     print('Enter the AC of the monster - if you want to plot it, enter -1')
     ac = int(input())
     print('Enter YES if your system includes doubling dmg on a critical hit ')
-    crit = input()
+    are_there_critical_hits = input()
     if ac != -1:
-        print('Expected damage per attack is ', expected_dmg_per_ac(to_hit, to_hit_mode, dmg, dmg_mode, crit, ac))
+        global accuracy
+        print('Expected damage per attack is ', expected_dmg_per_ac(to_hit, to_hit_mode, dmg, dmg_mode,
+                                                                    are_there_critical_hits, ac, accuracy))
     else:
         max_ac = 25
         arr = [0] * (max_ac + 1)
         for i in range(max_ac + 1):
-            arr[i] = expected_dmg_per_ac(to_hit, to_hit_mode, dmg, dmg_mode, crit, i)
+            global accuracy
+            arr[i] = expected_dmg_per_ac(to_hit, to_hit_mode, dmg, dmg_mode, are_there_critical_hits, i, accuracy)
         plt_probability(arr)
 
 
